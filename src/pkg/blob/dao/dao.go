6591d5b790742ceb9d9be37c5bc17bbc64c903d0
parag dudeja <parag.dudeja@razorpay.com>
2025-09-17 12:23:09 +0530

    union query

--- src/pkg/blob/dao/dao.go
+++ src/pkg/blob/dao/dao.go
@@ -17,6 +17,7 @@ package dao
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/docker/distribution/manifest/schema2"
@@ -245,23 +246,26 @@ func (d *dao) FindBlobsShouldUnassociatedWithProject(ctx context.Context, projec
 	if err != nil {
 		return nil, err
 	}
+	// Build UNION query for all blob digests with parentheses
+	var unionParts []string
+	var params []interface{}
 
-	sql := `SELECT b.digest_blob FROM artifact a JOIN artifact_blob b ON a.digest = b.digest_af WHERE a.project_id = ? AND b.digest_blob = ? LIMIT 1`
+	for _, blob := range blobs {
+		unionParts = append(unionParts, `(SELECT b.digest_blob FROM artifact a, artifact_blob b WHERE a.digest = b.digest_af AND a.project_id = ? AND b.digest_blob = ? LIMIT 1)`)
+		params = append(params, projectID, blob.Digest)
+	}
+
+	sql := strings.Join(unionParts, " UNION ")
+
+	var digests []string
+	_, err = o.Raw(sql, params...).QueryRows(&digests)
+	if err != nil {
+		return nil, err
+	}
 
 	shouldAssociated := map[string]bool{}
-
-	// Loop over each blob digest individually
-	for _, blob := range blobs {
-		var digests []string
-		_, err = o.Raw(sql, projectID, blob.Digest).QueryRows(&digests)
-		if err != nil {
-			return nil, err
-		}
-
-		// If any records are found, this blob should be associated with the project
-		if len(digests) > 0 {
-			shouldAssociated[blob.Digest] = true
-		}
+	for _, digest := range digests {
+		shouldAssociated[digest] = true
 	}
 
 	var results []*models.Blob
